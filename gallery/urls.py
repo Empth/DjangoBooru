02f145edcf530ddc87f21dd49ad2e4ed9a8ca1aa
emmitt <emmittchoi2002@gmail.com>
2024-09-23 14:57:26 -0700

    Views are now class based, clicking on the image in detail now opens up image in fullscreen

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from taggit.models import Tag
 
 from . import views
 
 app_name = "gallery"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:image_id>/", views.detail, name="detail"),
-    path("q=<str:query>/", views.filterIndex, name="filter_index"),
+    path("", views.IndexView.as_view(extra_context={'tags': Tag.objects.all()}), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("q=<str:query>/", views.FilterIndexView.as_view(extra_context={'tags': Tag.objects.all()}), name="filter_index")
 ]
+
+#     path("q=<str:query>/", views.filterIndex, name="filter_index"), # TODO
--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,44 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.urls import reverse
+from django.views import generic
 from .models import ImagePost
 from taggit.models import Tag
+from django.db.models import F
 
+class ExtraContext(object):
+    extra_context = {}
+
+    def get_context_data(self, **kwargs):
+        context = super(ExtraContext, self).get_context_data(**kwargs)
+        context.update(self.extra_context)
+        return context
+    
+
+class IndexView(generic.ListView, ExtraContext):
+    template_name = "gallery/index.html"
+    context_object_name = "latest_image_list"
+
+    def get_queryset(self):
+        """Return the last ten published image posts."""
+        return ImagePost.objects.order_by("-pub_date")[:10]
+    
+    
+class FilterIndexView(generic.ListView, ExtraContext):
+    template_name = "gallery/filter_index.html"
+    context_object_name = "latest_image_list"
+
+    def get_queryset(self):
+        # TODO Implement multi-tag filtering
+        """Return the last ten published image posts, filtered with tag 'query'."""
+        return ImagePost.objects.filter(tags__name__in=[self.kwargs['query']]).order_by("-pub_date")[:10]
+
+
+class DetailView(generic.DetailView):
+    model = ImagePost
+    template_name = "gallery/detail.html"
+
+'''
 def index(request):
     latest_image_list = ImagePost.objects.order_by("-pub_date")[:10]
     tags = Tag.objects.all()
@@ -19,3 +55,5 @@ def filterIndex(request, query: str):
     tags = Tag.objects.all()
     context = {"latest_image_list": latest_image_list, 'tags': tags}
     return render(request, 'gallery/filter_index.html', context)
+
+'''
